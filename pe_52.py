9da6968e55e94aee03691f28b1bdcfde824447a3
satani99 <42287151+satani99@users.noreply.github.com>
2022-07-05 21:52:24 +0530

    Create pe_52.py

---
+++ pe_52.py
@@ -0,0 +1,14 @@
+i = 1
+while True:
+  a = 2*i
+  b = 3*i
+  c = 4*i
+  d = 5*i
+  e = 6*i
+  if sorted(str(i)) == sorted(str(a)) == sorted(str(b)) == sorted(str(c)) == sorted(str(d)) == sorted(str(e)):
+    print(i)
+    break
+  i += 1
+  
+  
+# ans 142857
